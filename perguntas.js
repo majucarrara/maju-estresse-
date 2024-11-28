8cbfed5786ff4e2ca0ae84f9723d5f88fccb8d4d
majucarrara <mariajuliacarrara0905@gmail.com>
2024-11-28 14:24:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Filmes',
+    'Qual o melhor filme da Barbie?',
+    'Barbie: escola de princesas'
+)
+
+criaCartao(
+    'maju date',
+    'O que a Maju faria em um date?',
+    '"vou no banheiro" "mija na minha boca'
+)
+
+criaCartao(
+    'animal',
+    'Qual o animal favorito da Maju?',
+    'Banguela'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'pra onde o Marcio deveria ir?',
+    'pro caralho'
+)
